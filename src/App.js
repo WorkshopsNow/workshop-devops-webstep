2f328af61fd4e01794859f5c540b843b080f0d5a
Agron Kabashi <90187580+Agron-Kabashi@users.noreply.github.com>
2023-05-22 21:26:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hello
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Learned
+          Learn React
         </a>
       </header>
     </div>
